a5e1ff8563a5157fab93c72b1dd01e02f8a4814a
Nick Weinhold <n.c.weinhold@googlemail.com>
2014-01-07 23:10:35 +0000

    Creating the simple environment to hold built in functions.
    The environment is represented via a hash-table. For now, the
    environment is initialised with the 4 basic mathematical operators.
    
    In addition, two small wrapper functions for the stack have been
    created - just to wrap the send syntax for Racket classes.

--- forth.rkt
+++ forth.rkt
@@ -10,4 +10,28 @@
         top))
     (define/public (push v)
       (set! stack (cons v stack)))
+    (define/public (display-stack)
+      ;; Will have a nested function that takes a numeric argument
+      ;; The numeric value will just be the index into the stack
+      ;; e.g. 0 will represent the top of the stack
+      (display "TODO"))
     (super-new)))
+
+(define (pop-stack s)
+  (send s pop))
+
+(define (push-stack s v)
+  (send s push v))
+
+;; The environment for now will just be a global hash table...
+(define environment (make-hash))
+
+(define (add-to-env env k v)
+  (hash-set! env k v))
+
+;; Populate the global environment with some built in Forth operators / functions
+(define (initialise-environment env s)
+  (add-to-env env "+" (lambda () (push-stack s (+ (pop-stack s) (pop-stack s)))))
+  (add-to-env env "-" (lambda () (push-stack s (- (pop-stack s) (pop-stack s)))))
+  (add-to-env env "*" (lambda () (push-stack s (* (pop-stack s) (pop-stack s)))))
+  (add-to-env env "/" (lambda () (push-stack s (/ (pop-stack s) (pop-stack s))))))
