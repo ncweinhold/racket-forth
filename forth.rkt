e9234201aff80eb09cf8ea00603f946eaa6acb1a
Nick Weinhold <n.c.weinhold@googlemail.com>
2014-01-09 20:47:41 +0000

    Starting to process simple operations

--- forth.rkt
+++ forth.rkt
@@ -23,6 +23,7 @@
 (define (push-stack s v)
   (send s push v))
 
+(define stack (new Stack))
 ;; The environment for now will just be a global hash table...
 (define environment (make-hash))
 
@@ -34,4 +35,42 @@
   (add-to-env env "+" (lambda () (push-stack s (+ (pop-stack s) (pop-stack s)))))
   (add-to-env env "-" (lambda () (push-stack s (- (pop-stack s) (pop-stack s)))))
   (add-to-env env "*" (lambda () (push-stack s (* (pop-stack s) (pop-stack s)))))
-  (add-to-env env "/" (lambda () (push-stack s (/ (pop-stack s) (pop-stack s))))))
+  (add-to-env env "/" (lambda () (push-stack s (/ (pop-stack s) (pop-stack s)))))
+  (add-to-env env "." (lambda () (fprintf (current-output-port) "~a~n" (pop-stack s))))
+  (add-to-env env "BYE" (lambda () (exit))))
+
+(define (get-word k)
+  (hash-ref environment k))
+
+(define (parse-token token)
+  (if (string->number token)
+      (push-stack stack (string->number token))
+      (begin
+        ((get-word (string-upcase token)))
+        "ok")))
+
+(define (parse-line line)
+  (let ((tokens (string-split line)))
+    (for ([token tokens])
+      (parse-token token))))
+
+;; Repl function
+;; Read a line from the standard input.
+;; Break the line up into whitespace delimited tokens
+;; For each token, process it appropriately
+
+(define (repl)
+  (display ">>> ")
+  (let ((line (read-line)))
+    (parse-line line))
+  (repl))
+
+;; Main entry point
+
+(define (main)
+  (initialise-environment environment stack)
+  (display "Racket-Forth interpreter. Copyright (C) 2014, Nick Weinhold\n")
+  (display "Racket-Forth comes with ABSOLUTELY NO WARRANTY; Use at your own risk\n")
+  (repl))
+
+(main)
